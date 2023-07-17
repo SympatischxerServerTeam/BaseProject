d0a8950b8cb58d32ae7807a39f1050d773be8d19
Liyua <trqhxrd@duck.com>
2023-07-17 20:16:20 +0200

    Added base project

---
+++ src/main/java/com/github/sympatischxerserverteam/changeme/Main.java
@@ -0,0 +1,17 @@
+package com.github.sympatischxerserverteam.changeme;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
